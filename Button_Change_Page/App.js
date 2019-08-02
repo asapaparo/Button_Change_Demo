cd0bcd9373f7253889e0421db78e1575da30eb65
Arielle Cerini <arielle.cerini@createthenext.com>
2019-08-01 20:55:12 -0400

    Button_Change_Page
    Demo on how to use react native to change pages

---
+++ Button_Change_Page/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Button, View, Text } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+//to install react-navigation you can use npm install --save react-navigation
+
+class HomeScreen extends React.Component {//using class allows your to create different buttons on the screen, as well as render different information
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}//navigate to 'Details'
+        />
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
